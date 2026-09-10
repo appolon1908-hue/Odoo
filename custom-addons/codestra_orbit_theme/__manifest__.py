685af6bfd1cb5f33e0be80eecf650fd49e31fc5f
Ralph Appolon <appolon1908@gmail.com>
2026-09-09 21:37:35 -0400

    feat(sso): integrate Odoo with Keycloak using PKCE

--- custom-addons/codestra_orbit_theme/__manifest__.py
+++ custom-addons/codestra_orbit_theme/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Codestra Orbit Theme and SSO",
     "summary": "Supported Codestra shell and Keycloak OIDC for Odoo",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.1.0",
     "author": "Codestra",
     "website": "https://codestra.agency",
     "license": "LGPL-3",
